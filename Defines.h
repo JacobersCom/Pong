97ee6dd9517a2a68d964c2e0a95a636547c2d626
JacobersCom <jacobymartin1@icloud.com>
2025-11-12 23:59:59 -0500

    Clean up

--- Defines.h
+++ Defines.h
@@ -1,16 +1,13 @@
 #pragma once
-
 #include <iostream>
+
 #define TICKS_PASSED(A, B)  ((Sint32)((B) - (A)) <= 0)
 
 constexpr int screenH = 500;
 constexpr int screenW = 500;
 
-//Thickeness of wall
 constexpr int paddleW = 15;
 constexpr int paddleH = 100;
-constexpr float targetFPS = float(1000 / 60);
-
 
 struct vector2 
 {
